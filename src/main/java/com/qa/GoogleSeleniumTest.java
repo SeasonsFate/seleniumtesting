35a1011b836df5cb1908ca7ade45b593dffb20f8
SeasonsFate <alexbrew6@gmail.com>
2019-09-11 10:09:18 +0100

    updated chrome test

--- src/main/java/com/qa/GoogleSeleniumTest.java
+++ src/main/java/com/qa/GoogleSeleniumTest.java
@@ -1,11 +1,49 @@
 package com.qa;
 
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertTrue;
 
 public class GoogleSeleniumTest {
 
     private ChromeDriver driver;
 
+    @Before
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\IdeaProjects\\seleniumtesting\\src\\main\\java\\resources\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
 
 
+
+
+    @After
+    public void tearDown(){
+        driver.close();
+    }
+
+
+    @Test
+    public void searchTest2() throws InterruptedException {
+        driver.get("http://google.com");
+        Thread.sleep(2000);
+        WebElement searchField = driver.findElement(By.name("q"));
+        assertTrue(searchField.isDisplayed());
+        searchField.sendKeys("funny cat pictures");
+        Thread.sleep(5000);
+        WebElement submitButton = driver.findElement(By.name("btnK"));
+        submitButton.click();
+        Thread.sleep(5000);
+        WebElement linkToBiggerPicture = driver.findElementByLinkText("Images for funny cat");
+        linkToBiggerPicture.click();
+        Thread.sleep(5000);
+    }
 }
+
+
+
+
